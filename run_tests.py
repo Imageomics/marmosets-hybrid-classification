1b0a9e8dd929fdf76ce53ddc0fd860794100f115
David Carlyn <davidecarlyn@gmail.com>
2023-08-15 16:15:40 -0400

    Added function for color calibration

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
-from tests.test_color_calibration import test_get_color_card
+from tests.test_color_calibration import test_get_color_card, test_calibrate_images
 
 if __name__ == "__main__":
-    test_get_color_card()
+    #test_get_color_card()
+    test_calibrate_images()

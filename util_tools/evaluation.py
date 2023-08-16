04550bc3de9311096184364b609fb0e6a19633bb
David Carlyn <davidecarlyn@gmail.com>
2023-08-16 14:05:36 -0400

    Added column chart creation

--- util_tools/evaluation.py
+++ util_tools/evaluation.py
@@ -6,8 +6,10 @@ This includes creating graphics of these evaluations.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 
-from util_tools.general import to_numpy
+from util_tools.general import to_numpy, get_PIL_image_from_matplotlib_figure
 
 def calculate_accuracy(predictions, labels):
     """
@@ -18,6 +20,7 @@ def calculate_accuracy(predictions, labels):
     Description:
         Calculates the accuracy given predictions and labels
     """
+
     predictions = to_numpy(predictions)
     labels = to_numpy(labels)
     
@@ -25,4 +28,57 @@ def calculate_accuracy(predictions, labels):
     total = len(labels)
 
     return correct / total
+
+def calculate_per_class_accuracies(predictions, labels):
+    """
+    Args:
+        predictions ([int]): List of predictions
+        labels ([int]): List of labels/ground truths
+
+    Description:
+        Calculates the per class accuracies of the given predictions and labels
+    """
+
+    predictions = to_numpy(predictions)
+    labels = to_numpy(labels)
+    
+    correct_mask = (predictions == labels)
+
+    accuracies = []
+    for lbl in range(labels.max()+1):
+        lbl_mask = labels == lbl
+        lbl_total = lbl_mask.astype(np.int64).sum()
+        lbl_correct = (correct_mask & lbl_mask).astype(np.int64).sum()
+        acc = 0
+        if lbl_total > 0:
+            acc = lbl_correct / lbl_total
+        accuracies.append(acc)
+
+    return accuracies
+
+def create_accuracy_column_chart(predictions, labels, lbl_to_name_map=None):
+    """
+    Args:
+        predictions ([int]): List of predictions
+        labels ([int]): List of labels/ground truths
+        lbl_to_name_map (dict(int=>str)): Map from label to name/class
+        kwargs (): Key word arguments for matplotlib.pyplot.bar method
+
+    Description:
+        Creates column chart of per class accuracy.
+    """
+
+    accuracies = calculate_per_class_accuracies(predictions, labels)
+
+    names = [i if lbl_to_name_map is None else lbl_to_name_map[i] for i in range(len(accuracies))]
+
+
+    fig = plt.figure()
+    plt.title("Per Class Accuracies")
+    plt.bar(names, accuracies)
+    img = get_PIL_image_from_matplotlib_figure(fig)
+    plt.close()
+
+    return img
+
 
--- util_tools/general.py
+++ util_tools/general.py
@@ -3,9 +3,12 @@ General tools
 
 This script contains functions for general/misc. tools
 """
+import io
 
 import torch
 import numpy as np
+
+from PIL import Image
 
 def to_numpy(ten):
     if type(ten) is torch.Tensor:
@@ -14,3 +17,10 @@ def to_numpy(ten):
         return np.array(ten)
 
     return ten
+
+
+def get_PIL_image_from_matplotlib_figure(fig):
+    buf = io.BytesIO()
+    fig.savefig(buf)
+    buf.seek(0)
+    return Image.open(buf)

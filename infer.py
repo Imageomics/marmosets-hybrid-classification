45d4335617900bb44b45e2df965c666a822811a2
David Carlyn <davidecarlyn@gmail.com>
2023-08-30 14:54:06 -0400

    Update training an inference to account for GPU usage

--- infer.py
+++ infer.py
@@ -8,9 +8,9 @@ from util_tools.transforms import test_transforms
 def infer(image, model_path):
     model = tv_models.resnet34(weights=tv_models.ResNet34_Weights.IMAGENET1K_V1)
     model.fc = nn.Linear(512, 5)
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path, map_location="cpu"))
 
-    input_tensor = test_transforms(image).unsqueeze(0)
+    input_tensor = test_transforms()(image).unsqueeze(0)
 
     out = model(input_tensor)
     m = nn.Softmax(dim=1)
